ffa59da0e325b165b436a158631d64ebc8562eab
Mikhail Fast <mouseratti@gmail.com>
2015-01-26 01:32:57 +0600

    Updated: Valid banners list

--- Asterisk/Manager.py
+++ Asterisk/Manager.py
@@ -188,7 +188,8 @@ class BaseManager(Asterisk.Logging.InstanceLogger):
         'Asterisk Call Manager/1.1\r\n',
         'Asterisk Call Manager/1.2\r\n',
         'Asterisk Call Manager/1.3\r\n',
-        'Asterisk Call Manager/2.5.0\r\n'
+        'Asterisk Call Manager/2.5.0\r\n',
+        'Asterisk Call Manager/2.6.0\r\n',
     ]
 
 
